8286540e82fd1aa93f6ff8813b0b6e97ff43ddfd
Prithvi K <115770220+prithvikr@users.noreply.github.com>
2024-03-16 14:03:36 +0530

    Update Sidenav.jsx

--- components/Sidenav.jsx
+++ components/Sidenav.jsx
@@ -29,7 +29,7 @@ const Sidenav = () => {
                 <AiOutlineProject size={20} />
                 <span className='pl-4'>Projects</span>
               </a>
-              <a href="#main" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
+              <a href="technologies" className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'>
                 <BsPerson size={20} />
                 <span className='pl-4'>Resume</span>
               </a>
@@ -54,7 +54,7 @@ const Sidenav = () => {
           <a href="#projects" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
             <AiOutlineProject  size={20} />
           </a>
-          <a href="#main" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+          <a href="#technologies" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
             <BsPerson  size={20} />
           </a>
           <a href="#contact" className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
